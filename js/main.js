c269b26dad0a485616faf688efa1c355706b72e3
Constanza Pisano <constanza.pisano@gmail.com>
2021-10-14 16:57:00 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let caja=document.getElementById("irArriba");
+caja.addEventListener("click",function(){
+    document.documentElement.scrollTop=0;
+})
+
+window.addEventListener("scroll",function(){
+    if (document.documentElement.scrollTop > 0) {
+        caja.style.display="flex"
+    } else {
+        caja.style.display="none"
+    }
+})
